a0415ad0c76048660baa6385ba635d407a023a18
stylesuxx <stylesuxx@gmail.com>
2014-06-10 22:56:24 +0200

    Emit a test signal and then a quit_signal to shut down the receiver.

--- emitter.py
+++ emitter.py
@@ -11,6 +11,10 @@ class Emitter(dbus.service.Object):
 	def test(self):
 		print "Emitted a test signal"
 
+	@dbus.service.signal('tld.domain.sub.event')
+	def quit_signal(self):
+		print "Emitted a quit signal"
+
 """
 Emit a test signal on the dbus.
 Emit a receiver_quit signal which should stop the receiver.
@@ -19,3 +23,4 @@ session_bus = dbus.SessionBus()
 bus_name = dbus.service.BusName('sub.domain.tld', bus = session_bus)
 emitter = Emitter(bus_name, '/tld/domain/sub/event')
 emitter.test()
+emitter.quit_signal()
