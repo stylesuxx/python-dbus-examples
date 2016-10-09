43ae009a2aef82e548bd0f1f258ef191df77a374
Chris L <stylesuxx@gmail.com>
2016-10-09 02:34:35 +0200

    Fixed formatting

--- emitter.py
+++ emitter.py
@@ -4,23 +4,23 @@ import dbus.service
 import dbus.glib
 
 class Emitter(dbus.service.Object):
-	def __init__(self, bus_name, object_path):
-		dbus.service.Object.__init__(self, bus_name, object_path)
+    def __init__(self, bus_name, object_path):
+        dbus.service.Object.__init__(self, bus_name, object_path)
 
-	@dbus.service.signal('tld.domain.sub.event')
-	def test(self):
-		print "Emitted a test signal"
+    @dbus.service.signal('tld.domain.sub.event')
+    def test(self):
+        print 'Emitted a test signal'
 
-	@dbus.service.signal('tld.domain.sub.event')
-	def quit_signal(self):
-		print "Emitted a quit signal"
+    @dbus.service.signal('tld.domain.sub.event')
+    def quit_signal(self):
+        print 'Emitted a quit signal'
 
 """
 Emit a test signal on the dbus.
 Emit a receiver_quit signal which should stop the receiver.
 """
 session_bus = dbus.SessionBus()
-bus_name = dbus.service.BusName('sub.domain.tld', bus = session_bus)
+bus_name = dbus.service.BusName('sub.domain.tld', bus=session_bus)
 emitter = Emitter(bus_name, '/tld/domain/sub/event')
 emitter.test()
 emitter.quit_signal()
